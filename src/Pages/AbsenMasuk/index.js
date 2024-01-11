98b8adc1d985867d81d3b579ea83b338c1c7474a
MarioV27 <84635915+MarioV27@users.noreply.github.com>
2024-01-11 17:19:03 +0700

    Update index.js

--- src/Pages/AbsenMasuk/index.js
+++ src/Pages/AbsenMasuk/index.js
@@ -17,11 +17,35 @@ const AbsenMasuk = () => {
 
   const [selectedKamera, setSelectedKamera] = useState('');
 
-  const HandleAbsensi = screen => {
-    console.log(selectedKamera);
-    navigation.navigate('Menu');
-  };
+  const HandleAbsensi = async (e) => {
+    e.preventDefault();
 
+    const formData = new FormData();
+    formData.append("bukti", selectedKamera);
+    formData.append("lokasi", "kosong");
+    await axios
+      .post(`http://127.0.0.1:8000/api/absen`, formData)
+      .then(({ data }) => {
+        Swal.fire({
+          icon: "success",
+          text: "silahkan cek email anda untuk melakukan verifikasi akun anda",
+        });
+        navigate("/Menu");
+      })
+      .catch(({ response }) => {
+        if (response.status === 422) {
+          setValidationError(response.data.errors);
+
+          console.log("error");
+        } else {
+          Swal.fire({
+            text: response.data.message,
+            icon: "error",
+          });
+        }
+      });
+  };
+  
   return (
     <SafeAreaView style={styles.Container}>
       <View style={styles.Header}>
